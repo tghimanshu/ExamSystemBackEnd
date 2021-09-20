5bc2cbdc1231460ef7290b6a14a34b968ca68050
Himanshu <himanshugohil234@gmail.com>
2021-09-20 08:47:54 +0530

    solved some noto errors

--- functions/functions.php
+++ functions/functions.php
@@ -16,11 +16,11 @@ function getQuestionsFromExcel($fileUrl)
 		}
 		print_r($dups);
 		if (count($dups) != 0) {
-			$error="";
+			$error = "";
 			foreach ($dups as $key => $val) {
-				$error= $error."<h4 class='mb-0'>" . $val[0] . " repeated " . $val[1] . " times</h4> <br />";
+				$error = $error . "<h4 class='mb-0'>" . $val[0] . " repeated " . $val[1] . " times</h4> <br />";
 			}
-			header("Location: addpaper.php?errorQ=".$error);
+			header("Location: addpaper.php?errorQ=" . $error);
 		}
 		foreach ($xlsx->rows() as $r => $row) {
 			if ($i == 0) {
@@ -29,8 +29,11 @@ function getQuestionsFromExcel($fileUrl)
 			} else {
 				$answers = array($row[1], $row[2], $row[3], $row[5]);
 				foreach (array_count_values($answers) as $key => $c) {
-					if ($c > 1) header("Location: addpaper.php?errorO=<h1>Duplicate options in question no: " . $i);
+					if ($c > 1) {
+						header("Location: addpaper.php?errorO=<h1>Duplicate options in question no: " . $i);
+					}
 				}
+				die($c);
 				$jsonQ[$i - 1]['qId'] = $i - 1;
 				$jsonQ[$i - 1]['question'] = html_entity_decode($row[0]);
 				$jsonQ[$i - 1]['answer1'] = html_entity_decode($row[1]);
